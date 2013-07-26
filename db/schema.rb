4c8f4753c3acc307eb25611b4dfc0473a2af2748
peterfei <peterfeispace@gmail.com>
2013-07-26 17:12:23 +0800

    优化权限

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130725055823) do
+ActiveRecord::Schema.define(:version => 20130726083645) do
 
   create_table "airports", :force => true do |t|
     t.string  "name"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(:version => 20130725055823) do
     t.integer  "base_km"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+  end
+
+  create_table "call_centers", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "car_models", :force => true do |t|
@@ -317,8 +322,12 @@ ActiveRecord::Schema.define(:version => 20130725055823) do
     t.string   "phone"
     t.string   "password_digest"
     t.string   "email"
+    t.string   "uuid"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
   end
+
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
+  add_index "users", ["uuid"], :name => "index_users_on_uuid", :unique => true
 
 end
