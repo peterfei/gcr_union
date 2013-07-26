b7a36bf158c4250bbc718588e30f670b98957ebd
kikyous <kikyous@163.com>
2013-07-26 12:56:47 +0800

    后台车型数据维护

--- app/models/car_type.rb
+++ app/models/car_type.rb
@@ -1,12 +1,13 @@
 # coding: utf-8
 class CarType < ActiveRecord::Base
-  attr_accessible :big_package, :car_type_code, :car_type_img_url, :car_type_name, :description, :persons, :sequence, :small_package, :status
-  # mount_uploader :car_type_img_url, CompanyLogoUploader
+  attr_accessible :big_package, :car_type_code, :image, :car_type_name, :description, :persons, :sequence, :small_package, :status
+
+  mount_uploader :image, AttachmentUploader
 
   has_many :cars
   has_many :car_models
 
-  def to_s 
+  def to_s
     car_type_name
   end
 end
