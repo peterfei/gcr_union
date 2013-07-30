074f43273589610b229838e243f5695a64aefb25
peterfei <peterfeispace@gmail.com>
2013-07-30 14:41:38 +0800

    用户角色优化

--- app/models/manager_user.rb
+++ app/models/manager_user.rb
@@ -2,12 +2,22 @@
 class ManagerUser < ActiveRecord::Base
   attr_accessible :login_name, :manager_user_type, :password_digest,:password,:origin,:role
   has_secure_password 
-  ROLES = %w[admin  call_center oprator ]
+  ROLES = [['运营中心','admin'],  ['呼叫中心','call_center'],['加盟商', 'oprator' ]]
   def self.manager_user_type_list
     [['个人',0],['企业',1]]
   end
 
   def manager_user_type_text
     (ManagerUser.manager_user_type_list.find { |t,k|  k == self.manager_user_type.to_i } or []).first
+  end 
+  #REVIEW 页面取值 
+  self.attribute_names.each do |key| 
+    if /role$/=~key  
+      define_method("#{key}_text") do 
+        Hash[ROLES].invert[read_attribute(key.to_sym)]
+      end
+    end
   end
+
+
 end
