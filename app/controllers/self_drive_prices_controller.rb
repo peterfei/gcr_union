9155e32570711f3557665fa08d1dee09c51c8d24
kikyous <kikyous@163.com>
2013-08-07 16:58:11 +0800

    价格标志

--- app/controllers/self_drive_prices_controller.rb
+++ app/controllers/self_drive_prices_controller.rb
@@ -1,16 +1,2 @@
 class SelfDrivePricesController < ApplicationController
-  def date:date
-  end
-
-  def rate:decimal
-  end
-
-  def car_model:references
-  end
-
-  def location:references
-  end
-
-  def flag:string
-  end
 end
--- app/models/self_drive_prices.rb
+++ app/models/self_drive_prices.rb
@@ -2,4 +2,7 @@ class SelfDrivePrices < ActiveRecord::Base
   belongs_to :car_model
   belongs_to :location
   attr_accessible :date, :flag, :rate
+  extend Enumerize
+
+  enumerize :flag, in: [:weekday, :weekenday, :custome]
 end
