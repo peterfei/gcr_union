70814a4a8066aa5dc0b1988628b5751316538de1
kikyous <kikyous@163.com>
2013-08-22 10:09:39 +0800

    格式化代码

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,23 +1,27 @@
 # coding: utf-8
 class Location < ActiveRecord::Base
-  attr_accessible :address, :city_id, :district_id,:created_at,:updated_at, :end_time, :is24, :latitude, :location_name, :longitude, :phone, :phone, :principal, :principal_phone, :rate_code_list, :start_time, :status,:company_id
+  attr_accessible :address, :city_id, :district_id, :end_time, :is24,
+    :latitude, :location_name, :longitude, :phone, :phone, :principal,
+    :principal_phone, :rate_code_list, :start_time, :status,:company_id
+
   belongs_to :company
   belongs_to :city
   belongs_to :district
-  has_many :drivers
-  has_many :car_types
-  validates :location_name              , :presence => { :message => '请输入门店名称' }
-  validates :phone              , :presence => { :message => '请输入门店手机号码' }
-  validates :city_id              , :presence => { :message => '请输入门店所在城市' }
-  validates :district_id              , :presence => { :message => '请输入门店所在城市区域' }
-  validates :principal              , :presence => { :message => '请输入门店联系人姓名' }
-  validates :principal_phone              , :presence => { :message => '请输入门店联系人手机号码' }
-  validates :status              , :presence => { :message => '请输入门店工作状态' }
-  
+  has_many   :drivers
+  has_many   :car_types
+
+  validates :location_name,   presence: { message: '请输入门店名称' }
+  validates :phone,           presence: { message: '请输入门店手机号码' }
+  validates :city_id,         presence: { message: '请输入门店所在城市' }
+  validates :district_id,     presence: { message: '请输入门店所在城市区域' }
+  validates :principal,       presence: { message: '请输入门店联系人姓名' }
+  validates :principal_phone, presence: { message: '请输入门店联系人手机号码' }
+  validates :status,          presence: { message: '请输入门店工作状态' }
+
   extend Enumerize
   serialize :rate_code_list, Array
   enumerize :rate_code_list, in: {'日租'=>0,'时租'=>1,'接机'=>2,'送机'=>3,'自定路线'=>4}, multiple: true
-  
+
   def to_s
     location_name
   end
@@ -40,7 +44,7 @@ class Location < ActiveRecord::Base
     end
     h_list
   end
-  
+
   def self.minute_list
     m_list = {}
     (0..59).each do |min|
@@ -52,7 +56,7 @@ class Location < ActiveRecord::Base
     end
     m_list
   end
-  
+
   def status_text
     (Location.status_list.find { |t,k|  k == self.status.to_i } or []).first
   end
