7a0d6af6e87a5872081d237ff2918244f970e8bb
peterfei <peterfeispace@gmail.com>
2013-09-06 17:05:11 +0800

    fix reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -113,10 +113,11 @@ class ReservationsController < ApplicationController
   def dispatch_car  
     @reservation = Reservation.find(params[:id])
     #hot_fix delete seat in reservations
-    params[:reservation].delete('seat')
     respond_to do |format|  
     $o =false
      if request.put?  
+
+         params[:reservation].delete('seat')
         $o = @reservation.update_attributes(params[:reservation])   
         @reservation.car.update_attribute(:status,'disable')  
         @reservation.driver.update_attribute(:status,'disable')
