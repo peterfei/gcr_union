48a0394dfa7ba9286f3dde2623713e8d7ef4ef58
kikyous <kikyous@163.com>
2013-09-11 10:39:01 +0800

    价格添加验证

--- app/models/custom_price.rb
+++ app/models/custom_price.rb
@@ -1,4 +1,7 @@
 class CustomPrice < ActiveRecord::Base
   belongs_to :self_drive_price
   attr_accessible :price, :range
+
+  validates :range, presence: true
+  validates :price, presence: true
 end
