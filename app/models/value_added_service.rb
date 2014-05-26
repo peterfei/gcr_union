6ec62b7a131ecc1d5f260c4d25241254e224ef7f
kikyous <kikyous@163.com>
2014-05-26 14:57:52 +0800

    增值服务验证

--- app/models/value_added_service.rb
+++ app/models/value_added_service.rb
@@ -4,4 +4,10 @@ class ValueAddedService < ActiveRecord::Base
   extend Enumerize
   enumerize :_type, in: [:reservation, :self_driving], default: :reservation
   enumerize :status, in: [:enable, :disable], default: :enable
+
+  validates :name, presence: true, uniqueness: { scope: :_type }
+  validates :price, presence: true
+  validates :status, presence: true
+  validates :_type, presence: true
+
 end
