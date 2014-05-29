d2933e11f880e14730d1cfb9b1d2d52b5723da92
吴青 <wuqing@rongyitech.com>
2014-05-29 17:43:59 +0800

    修改部署文件

---
+++ config/deploy-xian.rb
@@ -0,0 +1,92 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
+# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
+
+set :domain, 'root@117.34.66.2 -p 34540'
+set :deploy_to, '/home/www/gcr_union'
+set :repository, '/repository/gcr_union.git'
+set :branch, 'master'
+
+# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
+# They will be linked in the 'deploy:link_shared_paths' step.
+set :shared_paths, ['config/database.yml', 'log', 'public',"tmp"]
+
+# Optional settings:
+#   set :user, 'foobar'    # Username in the server to SSH to.
+#   set :port, '30000'     # SSH port number.
+
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .rbenv-version to your repository.
+  # invoke :'rbenv:load'
+
+  # For those using RVM, use this to load an RVM version@gemset.
+  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+end
+
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
+task :setup => :environment do
+  queue! %[mkdir -p "#{repository}" && cd "#{repository}" && git init --bare]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/public"]
+
+  queue! %[ln -s /gcr_file_upload "#{deploy_to}/shared/public/uploads"]
+  queue! %[chown -R nobody:nobody "#{deploy_to}/shared/public/uploads"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/log"]
+  queue! %[mkdir -p "#{deploy_to}/shared/tmp"]
+
+  queue! %[chown -R nobody:nobody "#{deploy_to}/shared/log"]
+  queue! %[chown -R nobody:nobody "#{deploy_to}/shared/tmp"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/config"]
+
+  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
+  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
+
+end
+
+desc "Push repository to the remote server"
+task :before_clone do
+    system "git push rytech_platform_gcr_union #{branch}"
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    invoke :before_clone
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+
+    to :launch do
+      # queue "touch #{deploy_to}/tmp/restart.txt"
+      #queue "service httpd restart"
+	  #queue "script/delayed_job restart"
+    end
+  end
+end
+
+# For help in making your deploy script, see the Mina documentation:
+#
+#  - http://nadarei.co/mina
+#  - http://nadarei.co/mina/tasks
+#  - http://nadarei.co/mina/settings
+#  - http://nadarei.co/mina/helpers
+
