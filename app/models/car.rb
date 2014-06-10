76a79f6e96967e2d9e36b729630d8f9811abc627
kikyous <kikyous@163.com>
2014-06-10 13:31:30 +0800

    车辆数据导入

--- app/models/car.rb
+++ app/models/car.rb
@@ -29,4 +29,24 @@ class Car < ActiveRecord::Base
   def car_type_wr
     write_attribute :car_type_id, car_model.car_type_id
   end
+
+  def self.import(file)
+    spreadsheet = open_spreadsheet(file)
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      product = find_by_id(row["id"]) || new
+      product.attributes = row.to_hash.slice(*accessible_attributes)
+      product.save!
+    end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when ".csv" then Roo::CSV.new(file.path, nil, :ignore)
+    when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
+    when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+    else raise "不支持的文件类型: #{file.original_filename}"
+    end
+  end
 end
